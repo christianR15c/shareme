569b02e723cac236859738a288c32acd2eb2c3cc
Christian Habineza <habinezac15@gmail.com>
2023-01-06 10:16:33 +0200

    sanity schema defined

--- shareme_backend/schemas/index.js
+++ shareme_backend/schemas/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import user from './user'
+import pin from './pin'
+import save from './save'
+import comment from './comment'
+import postedBy from './postedBy'
+
+export const schemaTypes = [
+    user,
+    pin,
+    save,
+    comment,
+    postedBy
+]
